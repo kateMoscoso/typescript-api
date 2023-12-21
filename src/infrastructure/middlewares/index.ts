08ee40050155d40819b10c66af46a38b097906ac
kate <katiemscs@gmail.com>
2023-12-21 22:40:33 +0000

    add logger

--- src/infrastructure/middlewares/index.ts
+++ src/infrastructure/middlewares/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, urlencoded } from 'express';
+import morgan from 'morgan';
 
 import cors from 'cors';
 
@@ -6,6 +7,7 @@ function middlewares(app: Express) {
   app.use(cors());
   app.use(express.json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true }));
+  app.use(morgan('combined'));
 }
 
 export default middlewares;
